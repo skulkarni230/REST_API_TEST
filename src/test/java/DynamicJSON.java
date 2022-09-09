fdbff3131bd2c826225cede74f14e312a8b743bb
skulkarni230 <kulkarni.sohan230@gmail.com>
2022-09-09 10:00:11 +0530

    Providing inputs with data provider

--- src/test/java/DynamicJSON.java
+++ src/test/java/DynamicJSON.java
@@ -1,17 +1,40 @@
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import requests.payloads;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
 
 public class DynamicJSON {
 
-	@Test
-	public void addBook() {
+	@Test(dataProvider = "BookData")
+	public void addBook(String bookName, int lsbn, int aisle, String authorName) throws IOException {
 		RestAssured.baseURI = "http://216.10.245.166";
-		String response = given().header("Content-Type","application/json").body(payloads.addBook("Gita",2,001,"krishna"))
+
+		/*
+		* Sending data by calling payload
+		* */
+		String response = given().header("Content-Type","application/json").body(payloads.addBook(bookName,lsbn,aisle,authorName))
 				.when().post("Library/Addbook.php")
 				.then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+		/*
+		* Sending data by giving the path of JSON file just add files read all in body
+		*
+		String response = given().header("Content-Type","application/json").body(new String(Files.readAllBytes(Paths.get("c:/user"))))
+				.when().post("Library/Addbook.php")
+				.then().log().all().assertThat().statusCode(200).extract().response().asString();*/
+	}
+
+	@DataProvider(name = "BookData")
+	public Object[][] getData()
+	{
+		return new Object[][] {{"Wing_U",89,809,"Mark_U"}};
 	}
 }
